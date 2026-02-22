4a2927aa8b2af97ce38433ce6827def0bd65b887
Seidy <seidyalanis@gmail.com>
2026-02-22 17:45:00 -0600

    Implementación de las rutasput y delete para los vehículos existentes.

--- rutas/vehiculos.js
+++ rutas/vehiculos.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { crearVehiculo } = require("../controladores/vehiculo");
+const { 
+    crearVehiculo,
+    editarVehiculo,
+    eliminarVehiculo
+ } = require("../controladores/vehiculo");
 
 
 
 // Ruta para crear un nuevo vehículo
 router.post('/vehiculo', crearVehiculo)
+
+// Ruta para editar un vehículo existente
+router.put('/vehiculo/:id', editarVehiculo)
+
+// Ruta para eliminar un vehículo existente
+router.delete('/vehiculo/:id', eliminarVehiculo)
+
 
 module.exports = router;
 
