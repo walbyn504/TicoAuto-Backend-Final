d25e796c85a4fb47ae19ee21e0fd4d89c0c4293a
walbyn <walbyngonzalez@gmail.com>
2026-02-22 17:08:25 -0600

    Se creó el método de editar vehiculo

--- controladores/vehiculo.js
+++ controladores/vehiculo.js
@@ -18,6 +18,22 @@ const crearVehiculo = async (req, res) => {
     }
 };
 
+const editarVehiculo = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const body = req.body;
+        const opciopnes = { new: true };
+        const updatedVehiculo = await Vehiculo.findByIdAndUpdate(id, body, opciopnes);
+        if (!updatedVehiculo) { 
+            return res.status(404);
+        }
+        res.json(updatedVehiculo);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
 module.exports = {
-    crearVehiculo
+    crearVehiculo,
+    editarVehiculo
 };
