325641ef07adb8116e61a81f29a914311d515d3e
walbyn <walbyngonzalez@gmail.com>
2026-03-01 17:31:15 -0600

    Se crea el archivo y se translada método

---
+++ controladores/vehiculos/obtenerVehiculos.js
@@ -0,0 +1,29 @@
+const Vehiculo = require("../../modelos/vehiculo");
+
+
+const obtenerVehiculos = async (req, res) => {
+    try {
+        const vehiculos = await Vehiculo.find();    
+        res.status(200).json(vehiculos);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }       
+};
+
+const obtenerVehiculoPorId = async (req, res) => {  
+    try {
+        const id = req.params.id;
+        const vehiculo = await Vehiculo.findById(id);           
+        if (!vehiculo) {
+            return res.status(404);
+        }           
+        res.status(200).json(vehiculo);
+    }catch (error) {     
+
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    obtenerVehiculos, obtenerVehiculoPorId
+};
