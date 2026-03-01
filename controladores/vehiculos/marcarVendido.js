23fc7ea1573b642564b4f0e138ce3df56a0f19ea
walbyn <walbyngonzalez@gmail.com>
2026-03-01 17:30:58 -0600

    Se crea el archivo y se translada método

---
+++ controladores/vehiculos/marcarVendido.js
@@ -0,0 +1,27 @@
+const Vehiculo = require("../../modelos/vehiculo");
+
+const marcarVendido = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        // Actualiza solo el estado a "vendido"
+        const vehiculoActualizado = await Vehiculo.findByIdAndUpdate(
+            id,
+            { estado: 'vendido' },
+            { new: true } // devuelve el documento actualizado
+        );
+
+        if (!vehiculoActualizado) {
+           return res.status(404);
+        }
+
+        res.status(200).json(vehiculoActualizado);
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    marcarVendido
+};
