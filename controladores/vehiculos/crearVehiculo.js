83443dd54f7ab2beedb1b8becd6989ccf48d0e26
walbyn <walbyngonzalez@gmail.com>
2026-03-01 17:29:44 -0600

    Se crea el archivo y se translada método

---
+++ controladores/vehiculos/crearVehiculo.js
@@ -0,0 +1,26 @@
+const Vehiculo = require("../../modelos/vehiculo");
+
+const crearVehiculo = async (req, res) => {
+    try {
+        const vehiculo = new Vehiculo({
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            anno: req.body.anno,
+            precio: req.body.precio,
+            imagen: req.file.filename,
+            usuario: req.usuario._id
+        });
+
+        // Guardar el vehículo en la BD y retornar el vehículo creado
+        const vehiculoCreado = await vehiculo.save();
+        res.status(201).json(vehiculoCreado);
+        
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    crearVehiculo
+};
