af705621540f761c603e4ac765bb629985d03457
Seidy <seidyalanis@gmail.com>
2026-03-02 21:31:12 -0600

    Método para validar la seguridad de la contraseña e implementación en registrar usuario.

--- controladores/autenticacion.js
+++ controladores/autenticacion.js
@@ -1,6 +1,19 @@
 const usuario = require('../modelos/usuario');
 const bcrypt = require('bcrypt');
 
+
+const validarContrasenna = (contrasenna) => {
+
+    const tieneMin = /[a-z]/.test(contrasenna);
+    const tieneMay = /[A-Z]/.test(contrasenna);
+    const tieneNumero = /\d/.test(contrasenna);
+    const tieneEspecial = /[@$!%*?&.#_-]/.test(contrasenna);
+    const largoMinimo = contrasenna.length >= 8;
+
+    return tieneMin && tieneMay && tieneNumero && tieneEspecial && largoMinimo;
+};
+
+
 const registrarUsuario = async (req, res) => {
     const { nombre, primerApellido, segundoApellido, correo, contrasenna } = req.body;
 
@@ -9,6 +22,13 @@ const registrarUsuario = async (req, res) => {
         const usuarioExistente = await usuario.findOne({ correo });
         if (usuarioExistente) {
             return res.status(400).json({ message: "El correo electrónico ya está registrado." });
+        }
+
+        // Validación de seguridad
+        if (!validarContrasenna(contrasenna)) {
+            return res.status(400).json({
+                message: "La contraseña debe tener al menos 8 caracteres, incluir mayúscula, minúscula, número y carácter especial."
+            });
         }
 
         // Encriptar la contraseña
