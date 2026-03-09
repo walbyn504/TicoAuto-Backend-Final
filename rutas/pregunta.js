4134a8c61f22c25866db573239b3d400b9656914
walbyn <walbyngonzalez@gmail.com>
2026-03-08 21:59:12 -0600

    Se agrega la ruta de autenticacion

--- rutas/pregunta.js
+++ rutas/pregunta.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {crearPregunta} = required('../controladores/pregunta/crearPregunta');
+const { verificarToken } = require('../controladores/autenticacion/verificarToken');
+const crearPregunta = require('../controladores/pregunta/crearPregunta');
 
-router.post('/pregunta/crear', crearPregunta);
+router.post('/pregunta/crearPregunta', verificarToken, crearPregunta);
 
 module.exports = router;
