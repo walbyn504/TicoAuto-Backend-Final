60e2e957e1a9358d14d01fe15564a7fcfb6836b3
walbyn <walbyngonzalez@gmail.com>
2026-03-08 23:35:57 -0600

    Se agrega ruta de (ObtenerPreguntas)

--- rutas/pregunta.js
+++ rutas/pregunta.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const { verificarToken } = require('../controladores/autenticacion/verificarToken');
 const crearPregunta = require('../controladores/pregunta/crearPregunta');
+const obtenerPreguntas = require('../controladores/pregunta/obtenerPreguntas')
 
 router.post('/pregunta/crearPregunta', verificarToken, crearPregunta);
 
+router.get('/pregunta/obtenerMisPreguntas', verificarToken, obtenerPreguntas);
 module.exports = router;
