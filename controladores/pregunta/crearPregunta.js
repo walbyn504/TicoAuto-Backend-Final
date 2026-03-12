cf4857d11e1e4fc9503e83523e20a5b30a2fa8ef
walbyn <walbyngonzalez@gmail.com>
2026-03-12 16:32:24 -0600

    Se modifica para que no se pueda enviar dos preguntas seguidas

--- controladores/pregunta/crearPregunta.js
+++ controladores/pregunta/crearPregunta.js
@@ -1,11 +1,12 @@
 const Pregunta = require("../../modelos/pregunta");
+const Respuesta = require("../../modelos/respuesta");
 const Vehiculo = require("../../modelos/vehiculo");
 
 const crearPregunta = async (req, res) => {
     const { vehiculoId } = req.params;
     const { pregunta } = req.body;
 
-    if (!vehiculoId || !pregunta) {
+    if (!vehiculoId || !pregunta || !pregunta.trim()) {
         return res.status(400).json({
             mensaje: "El id del vehículo y la pregunta son requeridos."
         });
@@ -20,6 +21,30 @@ const crearPregunta = async (req, res) => {
             });
         }
 
+        
+
+        // Buscar la última pregunta que este usuario hizo sobre este vehículo
+        const ultimaPregunta = await Pregunta.findOne({
+            vehiculo: vehiculoId,
+            usuario: req.usuario.id
+        }).sort({ createdAt: -1 }); // Ordena por fecha de creación descendente
+
+
+        // SI hay una pregunta
+        if (ultimaPregunta) {
+            // Busca una respuesta asociada a la última pregunta
+            const respuestaExistente = await Respuesta.findOne({
+                pregunta: ultimaPregunta._id
+            });
+
+            // Si no existe respuesta, pregunta pendiente
+            if (!respuestaExistente) {
+                return res.status(400).json({
+                    mensaje: "No puedes enviar otra pregunta hasta que la anterior tenga respuesta."
+                });
+            }
+        }
+
         const nuevaPregunta = new Pregunta({
             vehiculo: vehiculoId,
             usuario: req.usuario.id,
@@ -28,10 +53,7 @@ const crearPregunta = async (req, res) => {
 
         const preguntaGuardada = await nuevaPregunta.save();
 
-        return res.status(201).json({
-            mensaje: "Pregunta creada correctamente.",
-            preguntaGuardada
-        });
+        return res.status(201).json({preguntaGuardada});
 
     } catch (error) {
         return res.status(500).json({
