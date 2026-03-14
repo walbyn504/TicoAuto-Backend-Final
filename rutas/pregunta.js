b7a889e2abb3991b56a7a77641bcefcb2db55ab2
walbyn <walbyngonzalez@gmail.com>
2026-03-14 15:23:21 -0600

    Se crea la ruta del controlador de obtener mis vehiuculos

--- rutas/pregunta.js
+++ rutas/pregunta.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const { verificarToken } = require('../controladores/autenticacion/verificarToken');
 const crearPregunta = require('../controladores/pregunta/crearPregunta');
 const obtenerPreguntas = require('../controladores/pregunta/obtenerPreguntas');
+const obtenerPreguntasVehiculos = require('../controladores/pregunta/obtenerPreguntasVehiculos');
 
 router.post('/vehiculo/:vehiculoId/pregunta', verificarToken, crearPregunta);
 router.get('/pregunta/obtenerMisPreguntas', verificarToken, obtenerPreguntas);
+router.get('/obtenerPreguntasDeMisVehiculos', verificarToken, obtenerPreguntasVehiculos);
 
 module.exports = router;
