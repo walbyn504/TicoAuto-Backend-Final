55702aa77c688ffe4e2281ad6e9f5ed0237d47c8
Seidy <seidyalanis@gmail.com>
2026-03-15 14:45:26 -0600

    Se mueve el archivo de obtenerPreguntasVehiculos a el controlador de conversación y así mismo, las rutas respectivas.

--- rutas/conversacion.js
+++ rutas/conversacion.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const { verificarToken } = require('../controladores/autenticacion/verificarToken');
 const obtenerPreguntasEnviadas = require('../controladores/conversacion/obtenerPreguntasEnviadas');
+const obtenerPreguntasVehiculos = require('../controladores/conversacion/obtenerPreguntasVehiculos');
 
 router.get('/pregunta/obtenerMisPreguntas', verificarToken, obtenerPreguntasEnviadas);
+
+router.get('/obtenerPreguntasDeMisVehiculos', verificarToken, obtenerPreguntasVehiculos);
+
 
 module.exports = router;
--- rutas/pregunta.js
+++ rutas/pregunta.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const { verificarToken } = require('../controladores/autenticacion/verificarToken');
 const crearPregunta = require('../controladores/pregunta/crearPregunta');
-const obtenerPreguntasVehiculos = require('../controladores/pregunta/obtenerPreguntasVehiculos');
+
 
 router.post('/vehiculo/:vehiculoId/pregunta', verificarToken, crearPregunta);
-router.get('/obtenerPreguntasDeMisVehiculos', verificarToken, obtenerPreguntasVehiculos);
+
 
 module.exports = router;
