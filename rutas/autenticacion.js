5dc95c6a549e7741d12caea049bf103c7d4ffb09
Seidy <seidyalanis@gmail.com>
2026-04-02 02:48:45 -0600

    Incluye ruta para verificación del correo.

--- rutas/autenticacion.js
+++ rutas/autenticacion.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 
-const {registrarUsuario} = require('../controladores/autenticacion/registrarUsuario');
-
+const {registrarUsuario } = require('../controladores/autenticacion/registrarUsuario');
 const {generarToken} = require('../controladores/autenticacion/generarToken');
+const {verificarCorreo} = require('../controladores/autenticacion/verificarCorreo');
   
         
 // Ruta para registrar un nuevo usuario
 router.post('/autenticacion', registrarUsuario);
 
 router.post('/autenticacion/login', generarToken);
+
+router.get('/autenticacion/verificacion', verificarCorreo);
 
 module.exports = router;
