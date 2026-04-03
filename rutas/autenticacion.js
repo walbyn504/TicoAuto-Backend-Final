552558805b5067ddb31f01c29f264c938f80ed58
walbyn <walbyngonzalez@gmail.com>
2026-04-02 21:00:30 -0600

    Se agregan las nuevas rutas

--- rutas/autenticacion.js
+++ rutas/autenticacion.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const {registrarUsuario } = require('../controladores/autenticacion/registrarUsuario');
 const {generarToken} = require('../controladores/autenticacion/generarToken');
 const {verificarCorreo} = require('../controladores/autenticacion/verificarCorreo');
+const {verificarCodigo2FA} = require('../controladores/autenticacion/verificarCodigo2FA');
   
         
 // Ruta para registrar un nuevo usuario
@@ -13,5 +14,8 @@ router.post('/autenticacion', registrarUsuario);
 router.post('/autenticacion/login', generarToken);
 
 router.get('/autenticacion/verificacion', verificarCorreo);
+
+// Verificación del código 2FA → aquí sí devuelve el JWT final
+router.post('/autenticacion/codigo2FA', verificarCodigo2FA);
 
 module.exports = router;
